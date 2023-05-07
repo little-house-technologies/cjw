6ccb2498bc81eef0414cdbc6b641e7e60ff63a6b
Gabriel Bissey <notsureiwantthispublic@email>
2023-05-06 19:53:28 -0600

    Bulk updates
    - Toggle mobile navbar
    - Update navbar links

--- javascript/controller.js
+++ javascript/controller.js
@@ -1,7 +1,26 @@
-const setupEventListeners = () => {
+const getCssPropertyById = (tag, property) => {
+  return getComputedStyle(document.getElementById(tag))[property];
+}
+
+const setCssPropertyById = (tag, property, value) => {
+  document.getElementById(tag).style[property] = value;
+}
+
+const listenForHamburgerMenuClick = () => {
   document.getElementById('hamburger-menu').addEventListener('click', () => {
-    console.log('menu clicked')
+    const display = getCssPropertyById('navbar-mobile', 'display');
+
+    if (display === 'none') {
+      setCssPropertyById('navbar-mobile', 'display', 'flex');
+    } else if (display === 'flex') {
+      setCssPropertyById('navbar-mobile', 'display', 'none');
+    }
+
   });
+}
+
+const setupEventListeners = () => {
+  listenForHamburgerMenuClick(); 
 }
 
 window.onload = () => {
