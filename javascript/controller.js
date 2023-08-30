a1d4cfaa4d64a05670eabcd5f8c0610ba368719a
Gabriel Bissey <notsureiwantthispublic@email>
2023-08-30 00:32:27 -0600

    Add slideshow

--- javascript/controller.js
+++ javascript/controller.js
@@ -74,8 +74,52 @@ const waitForLightbulbAnimationTrigger = () => {
 
 };
 
+
+
+let slideIndex = 1;
+// showSlides(slideIndex);
+
+// Next/previous controls
+const plusSlides = (n) => {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+const currentSlide = (n) => {
+  showSlides(slideIndex = n);
+}
+
+const showSlides = (n) => {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" dot-active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].className += " dot-active";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 window.onload = () => {
   setupEventListeners();
   highlightCurrentRoute();
   // waitForLightbulbAnimationTrigger();
+  showSlides(slideIndex);
 };
